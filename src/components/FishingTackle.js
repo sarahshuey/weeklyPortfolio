74d27785b093c435d8d1f0b96aca4232c45be3ed
Sarah Shuey <sarah.shuey@me.com>
2017-09-28 14:05:46 -0500

    images in

---
+++ src/components/FishingTackle.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import image from '../images/fishingbait.jpg'
+import image2 from '../images/tacklebox.jpg'
+import image3 from '../images/fishingrod.png'
+
+export default class Portfolio extends Component {
+  render(){
+    let style = {
+      "backgroundColor": "#bce9f4",
+      "backgroundSize": "cover",
+      "height": "100%",
+      "padding": "50px"
+    }
+    let subHeadings = {
+      "color": '#0d0056',
+      "textAlign":"center",
+      "textDecoration":"none"
+    }
+    return(
+      <div className="app-body findus offset col-lg-10 col-lg-offset-1">
+        <section className="row" style={style}>
+        <div className="image" style={{ display:"flex", flexDirection:"row", justifyContent:"center"}}>
+        <img style={{width: 250, height: 250, padding:"10px"}} src={image} alt="img"></img>
+          <img style={{width: 250, height: 250, padding:"10px"}} src={image2} alt="img"/>
+          <img style={{width: 250, height: 250, padding:"10px"}} src={image3} alt="img"/>
+          </div>
+          <div className="col-lg-8 map">
+            <a href="/" style={subHeadings}><p className="primary-text">Sarah Shuey Austin Tx</p></a>
+          </div>
+    </section>
+  </div>
+    )
+  }
+}
