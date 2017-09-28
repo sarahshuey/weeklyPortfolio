deb3787ad5774ac968d3d6cfd77a1ae4a73cbbe8
Sarah Shuey <sarah.shuey@me.com>
2017-09-28 13:45:59 -0500

    index.js page done

--- src/index.js
+++ src/index.js
@@ -4,5 +4,31 @@ import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import AboutUs from'./components/AboutUs';
+import BaseLayout from'./components/BaseLayout';
+import CampingNeeds from'./components/CampingNeeds';
+import ContactUs from'./components/ContactUs';
+import FishingTackle from'./components/FishingTackle';
+import HikingGear from'./components/HikingGear';
+
+
+
+ReactDOM.render(
+<BrowserRouter>
+   <BaseLayout>
+     <Switch>
+       <Route exact path="/" component={App} />
+       <Route path="/ContactUs" component={ContactUs} />
+       <Route path="/AboutUs" component={AboutUs} />
+       <Route path="/CampingNeeds" component={CampingNeeds} />
+       <Route path="/FishingTackle" component={FishingTackle} />
+       <Route path="/HikingGear" component={HikingGear} />
+     </Switch>
+   </BaseLayout>
+ </BrowserRouter>
+
+
+,document.getElementById('root'));
 registerServiceWorker();
